57e44c78c572d310d5cae79890fc9f9c91e75a83
Sameer <a.r.haider0207@gmail.com>
2025-02-26 06:04:47 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 123654
+let accountEmail = "sam@google.com"
+var accountPassword = "125478"
+
+accountCity = "Bhopal" // without variable use here
+
+let accountState
+
+console.log(accountId);
+
+// accountId=569874
+// console.log(accountId);  //bcz use const 
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+
+
