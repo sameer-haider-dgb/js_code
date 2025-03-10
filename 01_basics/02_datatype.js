54bc0c09dd5b6a4e0570635c02663e15d31dc9e2
Sameer <a.r.haider0207@gmail.com>
2025-03-10 05:58:43 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,24 @@
+let name= "Sameer"
+let no = 55
+let isLogin = true
+let arr1= [22, "Hello", false, 'A']
+let obj= {
+    firstName : "Abdul",
+    lastName : "Razzaque"
+}
+let x = null
+let y
+
+let sym = Symbol('Defination')
+let sym2= Symbol('Defination')
+
+console.log("Variable Value is "+name + " - And It's Data type is "+  (typeof name));
+console.log("Variable Value is "+no + " - And It's Data type is "+  (typeof no));
+console.log("Variable Value is "+isLogin + " - And It's Data type is "+  (typeof isLogin));
+console.log("Variable Value is "+arr1 + " - And It's Data type is "+  (typeof arr1));
+console.log("Variable Value is "+obj + " - And It's Data type is "+  (typeof obj));
+console.log("Variable Value is "+x + " - And It's Data type is "+  (typeof x));
+console.log("Variable Value is "+y + " - And It's Data type is "+  (typeof y));
+console.log((typeof  sym));
+console.log(sym === sym2);
+
