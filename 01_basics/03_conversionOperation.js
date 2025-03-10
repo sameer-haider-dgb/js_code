cd7461b791369642f940f3b29a8d8a43c60a59ab
Sameer <a.r.haider0207@gmail.com>
2025-03-10 08:10:29 +0000

    datatype conversoin

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let no = 66
+console.log(no +" "+ typeof no);
+
+let no2 = "66abd"
+console.log(no2 +" "+ typeof no);
+console.log("===================================");
+
+
+// conversion
+let stringInNmber = Number(no2 )    //Number is Conversion of data type
+console.log(typeof no2);
+
+console.log(stringInNmber);
+
+// Number
+// String
+//Boolean
+
+
