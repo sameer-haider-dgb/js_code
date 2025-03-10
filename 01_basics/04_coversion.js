a16fd09e81e9d4a49fa11cc6359c1dc077f1db73
Sameer <a.r.haider0207@gmail.com>
2025-03-10 09:25:38 +0000

    conversion commit

---
+++ 01_basics/04_coversion.js
@@ -0,0 +1,26 @@
+// console.log(2==2);
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2<=1);
+
+// console.log("2" == 2);
+// console.log("2" === 2);         // IMP Strict equality operator
+
+// Null Operator
+    console.log(null == 0);
+    console.log(null > 0);
+    console.log(null < 0);
+    console.log(null >= 0);         // imp  convert null into 0 number
+    console.log(null <= 0);         // imp
+
+// undefined
+    // console.log(undefined == 0);
+    // console.log(undefined > 0);
+    // console.log(undefined < 0);
+    // console.log(undefined >= 0);
+    // console.log(undefined <= 0);
+    
+
+
+
