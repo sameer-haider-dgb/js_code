efb6076a2b34127d004e2a671439f0003becb7ab
Sameer <a.r.haider0207@gmail.com>
2025-04-20 08:19:39 +0000

    object destructure 1

--- 02_basics/05_object_destruction.js
+++ 02_basics/05_object_destruction.js
@@ -16,17 +16,17 @@
     //  console.log(course.coursename);     // ObjectName.Key 
         // console.log(coursename);            // key Only
         
-// Object Default Values means can add NEW key value
+// Object Default Values means can add NEW key value like city = "Bengaluru"
         // const {price, courseInstructor, city = "Bengaluru"} = course; 
         // console.log(city);
 
-// Object Property Alias means RE_NAME Key
+// Object Property Alias means RE_NAME Key by :
         // const {price : coursePrice} = course; 
         // console.log(coursePrice);
 
 // String Destructuring means give text single value
         // const [a1,a2,a3,a4,a5] = coursename; // here use KEY_NAME Not Object
-        // console.log(a5);
+        // console.log(a5);                     // output s
         
         
 
