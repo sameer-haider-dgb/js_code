02be10b24021239665cc0df4c9c8db3ff017d1ad
Sameer <a.r.haider0207@gmail.com>
2025-04-20 08:41:00 +0000

    again destructure

--- 02_basics/06_object_destruction.js
+++ 02_basics/06_object_destruction.js
@@ -2,27 +2,27 @@
         const cars  = ["Punch", "Nexon", "Celereo", "Swift","Brezza"]
 
         // const [c1, c2] = cars
-        // console.log(c2);
+        // console.log(c2);             // Output = Nexon
 
 // Skipping Array Values by using  , 
         // const [c1,c2,,,c3 ] = cars          // print 5 position value but write c3 not c5
-        // console.log(c3);
+        // console.log(c3);             // Output = Brezza
 
 // Array Position Values gives INDEX
-        const 
-        {  [0] : c1, [3] : c2 }  = cars
-        // console.log(c2);
+        // const 
+        // {  [0] : c1, [3] : c2 }  = cars
+        // console.log(c2);                        // Output = Swift
         
 // The Rest Property using spread concept ...
-        const numbers = [10, 20, 30, 40, 50, 60, 70];
-        const [a,b, ...rest] = numbers
-        // console.log(rest);
+        // const numbers = [10, 20, 30, 40, 50, 60, 70];
+        // const [a,b, ...rest] = numbers
+        // console.log(rest);                       // Output = [30, 40, 50, 60, 70]
 
 // Swapping JavaScript Variables
-        let firstName = "Sameer";
-        let lastName = "Haider";
-        [firstName, lastName] = [lastName, firstName]; // alti palti
-        console.log(firstName);
+        // let firstName = "Sameer";
+        // let lastName = "Haider";
+        // [firstName, lastName] = [lastName, firstName]; // alti palti
+        // console.log(firstName);                         // Output = Haider
 
 // Destructuring Maps
         // Create a Map
@@ -37,7 +37,7 @@
         for (const [key, value] of fruits) {
             text += key + " is " + value +", ";
         }
-        console.log(text);
+        console.log(text);                  // Output = apples is 500, bananas is 300, oranges is 200, 
         
         
 
