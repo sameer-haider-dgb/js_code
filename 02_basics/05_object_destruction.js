35e6e815b849bb06cc94b2cdcb13629210895f81
Sameer <a.r.haider0207@gmail.com>
2025-04-20 07:54:24 +0000

    Object De_structre

---
+++ 02_basics/05_object_destruction.js
@@ -0,0 +1,5 @@
+// Destructuring is not destructive.
+// Destructuring does not change the original object.
+// let {firstName, lastName} = person;
+// let [firstName, lastName] = person;      // both same
+
