04267905feef1f1e1709b6d46bb06dcd57fd88ba
Sameer <a.r.haider0207@gmail.com>
2025-04-30 04:39:51 +0000

    changes in arrow function

--- 03_basics/05_arrow_function.js
+++ 03_basics/05_arrow_function.js
@@ -15,25 +15,29 @@
         // console.log(this);
 // ==================================================================
         function chai_1()
-        {
-        //     console.log(this);       // Give big info
-        }
+            {
+            //     console.log(this);       // Give big info
+            }
         chai_1()
 
-    // i)    
+    // i)      THIS refers to the global object
+    //          Variables declared with let, const, or var inside a function do not get attached to the global object due to using THIS
                 function chai_2()
-                {
-                    let username = "Rashid"
-                //     console.log(this.username);         // undefined
-                }
+                    {
+                        let username = "Rashid"          // Local object
+                        console.log(this.username);    // Output      undefined       [Bcz this.username looks for a property called username on the global object — but it doesn't find one, hence undefined]
+                        console.log(username);          //  Output      Rashid
+                    }
                 chai_2()
      // ii)
-            const chai_3 = function()           // Called Expression function
-            {
-                let username = "Rashid"
-                // console.log(this.username);         // undefined
-            }
+            let username = "Rashid"             // Global object
+                const chai_3 = function()           // Called Expression function
+                {
+                    // console.log(this.username);         // Output        Rashid
+                }
             chai_3()   
+
+// =======================================================================
 // ARROW Function by      ()  =>  {      }
     // Arrow function without parameter
             const chai_4 = () =>  {
@@ -44,23 +48,23 @@
             chai_4()   
 // ------------------------------------------------------------------------------------
  // Arrow function with parameter
-     // i) Explicitely means use RETURN
-        const addTwo = (n1, n2) => { return n1 + n2;}
-        // console.log(addTwo(5,9));           // Output           14
+     // i) Explicitly means use RETURN
+                const addTwo = (n1, n2) => { return n1 + n2;}
+                // console.log(addTwo(5,9));           // Output           14
 
-    // ii) Implicitely return means no need RETURN and {     } 
-        // const subTwo = (n1, n2) =>  n1 - n2; 
-        const subTwo = (n1, n2) =>  (n1 - n2);                          // due to single line of code
+    // ii) Implicitly return means no need RETURN and {     } 
+                // const subTwo = (n1, n2) =>  n1 - n2; 
+                const subTwo = (n1, n2) =>  (n1 - n2);                          // due to single line of code
 
-        // const subTwo = (n1, n2) =>  {username : "Shyam"};           /// Use Object {} give error bcz () is imp
-        const objectUse = (n1, n2) =>  ( {username : "Shyam"} );           /// Use Object 
+                // const subTwo = (n1, n2) =>  {username : "Shyam"};           /// Use Object {} give error bcz () is imp
+                const objectUse = (n1, n2) =>  ( {username : "Shyam"} );           /// Use Object 
 
-        console.log(subTwo(9,5));
-        console.log(objectUse());
+                // console.log(subTwo(9,5));
+                // console.log(objectUse());
 
 // ------------------------------------------------------------------------------
             const myArr =  [8,3,8,9,2]
-            myArr.forEach(function( ) {     }       )
-            myArr.forEach(             ( ) =>  {     }     )              // need RETURN
-            myArr.forEach(             ( ) =>  console.log(    )       )             // No Need RETURN for bcz of singleline of code
+                // myArr.forEach(function( ) {     }       )
+                // myArr.forEach(             ( ) =>  {     }     )              // need RETURN
+                // myArr.forEach(             ( ) =>  console.log(    )       )             // No Need RETURN for bcz of singleline of code
         
