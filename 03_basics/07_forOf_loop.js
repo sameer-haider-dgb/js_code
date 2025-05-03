659d459a62bb4f994ad41585152f25014847154d
Sameer <a.r.haider0207@gmail.com>
2025-05-03 18:04:35 +0000

    changes

--- 03_basics/07_forOf_loop.js
+++ 03_basics/07_forOf_loop.js
@@ -2,5 +2,16 @@
 // Benefit is we do not require length of variable
         const arr = [123, "Sameer", 'A', true];
         for (const a of arr) {
-            console.log(a);
+            // console.log(a);
         }
+
+        const name = "Md Abdul Razzaque";
+        for (const n of name) {
+            if (n==" ") {
+                continue;
+            }
+            // console.log(n);  
+        }
+
+        const myMap = new Map();
+        my
