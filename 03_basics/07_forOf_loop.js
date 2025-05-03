8f82691be3109cb47ead787a6ed4989261f5b3ec
Sameer <a.r.haider0207@gmail.com>
2025-05-03 17:53:30 +0000

    for Of Loop understand

---
+++ 03_basics/07_forOf_loop.js
@@ -0,0 +1,6 @@
+// forOf Loop
+// Benefit is we do not require length of variable
+        const arr = [123, "Sameer", 'A', true];
+        for (const a of arr) {
+            console.log(a);
+        }
